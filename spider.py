b163bd0e477d18c7b8807a474c1054b58a8d2438
LuckyAbby <luckyabby97@gmail.com>
2017-12-10 15:01:02 +0800

    update

--- spider.py
+++ spider.py
@@ -8,18 +8,14 @@ def get_urls():
     res = requests.get(root_url)
     soup = bs4.BeautifulSoup(res.text, "html.parser")
     url_list = soup.find_all('div', class_='info')
-    # print(url_list)
+    print(url_list)
     for item in url_list:
-        # print(item)
-        title = item.a.attrs.get('title')
-        # .attrs.get('title')
-        # print('title is', title)
-        url = item.find('div', class_='address').string
-        # url = item.
-        # .string
-        print('url is', url)
-        # url_info_list.append({
-        # 'title':title, 'url':url
-        # })
+        if item.find('div', class_='title'):
+            title = item.a.attrs.get('title')
+        if item.find('div', class_='address'):
+            url = item.find('div', class_='address').string
+        url_info_list.append({
+        'title':title, 'url':url
+        })
     return url_info_list
 print(get_urls())
